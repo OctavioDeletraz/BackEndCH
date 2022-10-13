d3079304b37cc0bad5218740cee7280e265325af
DELETRAZ Octavio <octavio.deletraz@gmail.com>
2022-10-13 10:51:32 -0300

    iniciazando repo

---
+++ Deletraz.js
@@ -0,0 +1,61 @@
+class User {
+    constructor(name, lastName, books, pets) {
+        this.name = name,
+            this.lastName = lastName,
+            this.books = books,
+            this.pets = pets
+    }
+
+    getFullName() {
+        return `${this.name} ${this.lastName}`
+    }
+
+    addPet(pet) {
+        this.pets.push(pet)
+    }
+
+    countPets() {
+        return this.pets.length
+    }
+
+    addBook(title, author) {
+        this.books.push({ title: title, author: author })
+    }
+
+    getBookNames() {
+        let bookNames = this.books.map(elem => elem.title)
+        return bookNames
+    }
+}
+
+let octaviobooks = [
+    {
+        title: "El gato negro",
+        author: "Edgar Allan Poe"
+    },
+    {
+        title: "El cuervo",
+        author: "Edgar Allan Poe"
+    },
+    {
+        title: "El corazon delator",
+        author: "Edgar Allan Poe"
+    }
+]
+
+const userOctavio = new User("Octavio", "Deletraz", octaviobooks, [])
+
+console.log(userOctavio);
+
+console.log(userOctavio.getFullName())
+
+userOctavio.addPet("Polola")
+userOctavio.addPet("Senna")
+console.log(userOctavio.pets)
+
+console.log(userOctavio.countPets())
+
+userOctavio.addBook("El solitario", "Horacio Quiroga")
+console.log(userOctavio.books)
+
+console.log(userOctavio.getBookNames())
