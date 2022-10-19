066edd20f48f482cf79c02de8fadd8742c6ae511
DELETRAZ Octavio <octavio.deletraz@gmail.com>
2022-10-19 10:29:00 -0300

    correcciones

--- Desafio2/Index.js
+++ Desafio2/Index.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 
 class Contenedor {
 
-    // constructor(route) {
-    //     this.route = route
-    // }
+    constructor(route) {
+        this.route = route
+    }
 
-    async getAll(file) {
+    async getAll() {
         let allProductsArray = []
         try {
-            let allProductsString = fs.readFileSync(file, "utf8");
+            let allProductsString = fs.readFileSync(`./${this.route}`, "utf8");
             allProductsString.length > 0
                 ? (allProductsArray = JSON.parse(allProductsString))
                 : (allProductsArray = []);
@@ -19,24 +19,24 @@ class Contenedor {
         return allProductsArray
     }
 
-    async write(allProductsArray, file) {
+    async write(allProductsArray) {
         let allProductsString = JSON.stringify(allProductsArray);
         try {
-            await fs.writeFileSync(file, allProductsString);
+            await fs.writeFileSync(`./${this.route}`, allProductsString);
         } catch (error) {
             console.log("Error de escritura", error);
         }
     }
 
-    async deleteById(id, file) {
+    async deleteById(id) {
         try {
-            const products = await this.getAll(file)
+            const products = await this.getAll()
             const productsFiltered = products.filter(elem => elem.id !== id)
             if (products.length === productsFiltered.length) {
                 console.log("No existe el id")
                 return null
             } else {
-                this.write(productsFiltered, file)
+                this.write(productsFiltered)
                 console.log(`Se elimino correctamente el producto de id: ${id}`)
             }
         } catch (error) {
@@ -44,47 +44,51 @@ class Contenedor {
         }
     }
 
-    async getById(id, file) {
+    async getById(id) {
         try {
-            const products = await this.getAll(file)
-            const productsFiltered = products.filter(elem => elem.id == id) ?? null
+            const products = await this.getAll()
+            const productsFiltered = products.filter(elem => elem.id == id)
+            if (productsFiltered.length == 0) {
+                console.log("Id inexistente")
+            }
             return productsFiltered
         } catch (error) {
             console.log(error)
         }
     }
 
-    async deleteAll(file) {
+    async deleteAll() {
         try {
-            fs.writeFileSync(file, JSON.stringify([]))
+            fs.writeFileSync(`./${this.route}`, JSON.stringify([]))
+            console.log("Se eliminaron todos los registros")
         } catch (error) {
             console.log(`No se pudieron eliminar los productos ${error}`)
         }
     }
 
-    async getNextId(file) {
+    async getNextId() {
         try {
-            let products = await this.getAll(file);
-            let nextId = parseInt(products[products.length - 1].id) + 1;
-            if (products.filter(elem => elem.id === nextId).length == 0) {
-                return nextId
+            let products = await this.getAll();
+            let nextId
+            if (products.length > 0) {
+                nextId = parseInt(products[products.length - 1].id) + 1;
             } else {
-                console.log("No se pudo obtener id")
-                return []
+                nextId = 1
             }
+            return nextId
         } catch (error) {
             console.log("No se pudo obtener id", error)
         }
 
     }
 
-    async save(product, file) {
+    async save(product) {
         console.log("Guardando...", product)
-        let nextId = await this.getNextId(file)
+        let nextId = await this.getNextId()
         product.id = nextId
-        const allProductsArray = await this.getAll(file)
+        const allProductsArray = await this.getAll()
         allProductsArray.push(product)
-        this.write(allProductsArray, file)
+        this.write(allProductsArray, `./${this.route}`)
     }
 
 }
--- Desafio2/Pruebas.js
+++ Desafio2/Pruebas.js
@@ -3,22 +3,20 @@ const Contenedor = require('./Index')
 async function main() {
     const products = new Contenedor('products.json')
 
-    const file = "./Products.json"
-
     // Prueba para obtener todos los productos
-    console.log(await products.getAll(file))
+    console.log(await products.getAll())
 
     // Prueba para borrar un producto por id, y mostrar resultados en pantalla
-    await products.deleteById(3, file)
-    console.log(await products.getAll(file))
+    await products.deleteById(8)
+    console.log(await products.getAll())
 
     // Prueba para obtener un producto especifico por id, y mostrarlo por pantalla
-    const productId = await products.getById(2, file)
+    const productId = await products.getById(1)
     console.log(productId)
 
     // Prueba para eliminar todos los productos y mostrar el array vacio
-    await products.deleteAll(file)
-    console.log(await products.getAll(file))
+    await products.deleteAll()
+    console.log(await products.getAll())
 
 
     // Nuevo objeto para agregar al array de productos
@@ -29,7 +27,7 @@ async function main() {
     }
 
     // Prueba para guardar un nuevo producto en la base de datos
-    await products.save(newProduct, file)
-    console.log(await products.getAll(file))
+    await products.save(newProduct,)
+    console.log(await products.getAll())
 }
 main()
