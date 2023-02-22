2a6202cc0fa4357851c66d8d651cd19e4e5cbe70
octavio deletraz <octavio.deletraz@gmail.com>
2023-02-22 20:53:35 -0300

    capas

--- LoginRegister/src/db/dbConnection.js
+++ LoginRegister/src/db/dbConnection.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-const dbConnection = mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@ecommercecoderhousesant.6p5agbc.mongodb.net/sessions`, {
+const dbConnection = mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clusteroctavio.nugxlnp.mongodb.net/sessions`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000
 })
-.then(()=> console.log('Conectado a Mongodb'))
-.catch(err => console.log(err))
+    .then(() => console.log('Conectado a Mongodb'))
+    .catch(err => console.log(err))
 
 
 module.exports = dbConnection
--- LoginRegister/src/db/sessionDBConnection.js
+++ LoginRegister/src/db/sessionDBConnection.js
@@ -1,12 +1,12 @@
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const advancedOptions = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }
 const sessionDBConnection = session({
     store: MongoStore.create({
-        mongoUrl: `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@ecommercecoderhousesant.6p5agbc.mongodb.net/sessions?retryWrites=true&w=majority`,
+        mongoUrl: `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clusteroctavio.nugxlnp.mongodb.net/sessions?retryWrites=true&w=majority`,
         mongoOptions: advancedOptions
     }),
     secret: 'algo',
